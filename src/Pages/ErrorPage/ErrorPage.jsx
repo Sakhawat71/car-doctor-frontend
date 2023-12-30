ced0f2f0954ac1f3557b912522abf8bebfc54277
sakhawat71 <sakhawat.showrav@gmail.com>
2023-12-31 00:40:36 +0600

    layout, pages and error element

---
+++ src/Pages/ErrorPage/ErrorPage.jsx
@@ -0,0 +1,13 @@
+
+const ErrorPage = () => {
+
+    
+    return (
+        <div>
+            <h1 className="text-red-600 text-7xl">404</h1>
+            <h2 className="text-red-700 italic">Page not found</h2>
+        </div>
+    );
+};
+
+export default ErrorPage;
--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -1,17 +1,26 @@
-const { createBrowserRouter } = require("react-router-dom");
+import { createBrowserRouter } from "react-router-dom";
+import Root from "../Layout/Root";
+import Home from "../Pages/Home/Home";
+import ErrorPage from "../Pages/ErrorPage/ErrorPage";
+
 
 
 const router = createBrowserRouter([
     {
-        path: '/',
-        element: <></>,
-        errorElement: <></>,
+        path: "/",
+        element: <Root></Root>,
+        errorElement: <ErrorPage></ErrorPage>,
         children: [
             {
-
+                path: "/",
+                element: <Home></Home>
+            },
+            {
+                path: "/services",
+                element:
             }
         ]
-    }
+    },
 ])
 
 export default router;
