5b89486f38d006270a8057983a6539ba3a31ea7d
sakhawat71 <sakhawat.showrav@gmail.com>
2023-12-31 05:11:21 +0600

    create footer,navbar and add assets

--- src/Layout/Root.jsx
+++ src/Layout/Root.jsx
@@ -1,9 +1,13 @@
 import { Outlet } from "react-router-dom";
+import Footer from "../Shared/Footer/Footer";
+import Navbar from "../Shared/Navbar/Navbar";
 
 const Root = () => {
     return (
         <div>
+            <Navbar></Navbar>
             <Outlet></Outlet>
+            <Footer></Footer>
         </div>
     );
 };
