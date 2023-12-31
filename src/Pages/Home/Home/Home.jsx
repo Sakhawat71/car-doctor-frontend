870faf7b7cd5a6d44c0445cf59b0daf92153003e
sakhawat71 <sakhawat.showrav@gmail.com>
2024-01-01 01:20:32 +0600

    banner pic added

--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -1,9 +1,12 @@
+import Banner from "../Banner/Banner";
+
 const Home = () => {
 
 
     return (
         <div>
-            <h1>Welcome to Home</h1>            
+            <h1>Welcome to Home</h1>   
+            <Banner></Banner>         
         </div>
     );
 };
